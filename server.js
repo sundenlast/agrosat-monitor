26ff8d0705f16f61eebe0dac455a59f4bc81e592
Enzo <photopsia@proton.me>
2026-06-09 16:34:34 -0300

    Teste servidor Node

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname));
+app.get('/', (req, res) => {
+    res.send('<h1>AgroSat Monitor funcionando!</h1>');
+});
 
 const PORT = process.env.PORT || 8080;
 
